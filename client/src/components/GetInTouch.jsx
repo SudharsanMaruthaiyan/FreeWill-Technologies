6b4f1028938fe9630ac07a09ceed72ca0cfb9581
Honey-Joe <honeyjoe942@gmail.com>
2025-04-19 00:57:23 +0530

    Updated Get in Touch

--- client/src/components/GetInTouch.jsx
+++ client/src/components/GetInTouch.jsx
@@ -29,7 +29,7 @@ const GetInTouch = () => {
       <div className="md:w-2/5 bg-[url(https://ik.imagekit.io/HoneyJoe/freewill%20technologies%20assetss/rag-doll-with-giant-magnifying-glass-looking-blue.jpg?updatedAt=1745003532644)] bg-cover text-white px-8 relative rounded-[5%]">
         <div className="absolute inset-0 bg-black opacity-30 rounded-[5%]"></div>
 
-        <div className="relative z-10 h-full flex flex-col justify-center space-y-12">
+        <div className="relative z-10 h-full flex flex-col justify-center space-y-12 py-10">
           <div className="space-y-2">
             <div className="flex items-center">
               <Mail className="mr-4" size={24} />
