990330c15b270f8fbe2be24ed7f8b3385eee2fc5
Honey-Joe <honeyjoe942@gmail.com>
2025-04-23 19:28:13 +0530

    updated router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Service from "./pages/Service";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Service from "./pages/Service";
-// import Navbar from "./components/Navbar";
-
-const App = () => (
-  <>
-    {/* <div className=" w-[100%] px-[2%] xl:w-full xl:px-[5%] mx-auto sticky top-0 bg-white">
-      <Navbar />
-    </div> */}
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/service" element={<Service />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  </>
-);
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
